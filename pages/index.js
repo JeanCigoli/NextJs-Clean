b20015ca1fe84d0a99e4239bc014e5d4725b2c2e
jeancigoli <jean@primi.com.br>
2020-11-29 19:22:50 -0300

    fix: for build

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,17 @@
 import Head from '../src/infra/components/Header';
-import { Container } from './_styled';
 import Header from '../src/patterns/Header';
 import About from '../src/patterns/About';
 import Projects from '../src/patterns/Projects';
 import Contact from '../src/patterns/Contact';
 import Footer from '../src/patterns/Footer';
+
+import styled from 'styled-components';
+
+const Container = styled.div`
+  width: 100%;
+  min-height: 500px;
+  height: auto;
+`;
 
 const Home = () => {
   return (
